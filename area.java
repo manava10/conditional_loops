085f87e7d74606fcba514d2bc181e73a078954cf
Manava <114528094+manava10@users.noreply.github.com>
2023-08-03 17:47:22 +0530

    Create area.java
    Area of circle.

---
+++ area.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class area {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of circle");
+        int radius = sc.nextInt();
+        int area = (int) (3.14*radius*radius);
+        System.out.println("The area of circle with given radius will be"+ " " + area);
+
+
+
+
+
+    }
+    
+}
