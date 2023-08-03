f3fdcc5d70df55858e763675fcd64f4de214626b
Manava <114528094+manava10@users.noreply.github.com>
2023-08-03 22:33:21 +0530

    Create Star_Pattern2.java
    Inverted Star Pattern

---
+++ Star_Pattern2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Star_Pattern2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+        for(int i=n; i>=1; i--){
+            for(int j=i; j>=1; j--){
+                System.out.print("*");
+            }System.out.println();
+        }
+    }
+}
